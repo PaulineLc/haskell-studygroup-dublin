3cdef415a84119dea2e08ab17f1288fb114b9275
Pauline Lallinec <PaulineLc@users.noreply.github.com>
2017-09-30 18:46:02 +0100

    answers to chapter 1

---
+++ src/chapter1.hs
@@ -0,0 +1,29 @@
+{-|
+  Chapter 1:
+
+  Intermission exercice:
+  1. B.
+  2. C
+  3. B
+
+  Combinators
+  1. Y
+  2. N
+  3. Y
+  4. Y
+  5. N
+
+  Normal form vs diverge
+  1. Normal form
+  2. Diverge
+  3. Normal form (zzz)
+
+  Beta reduce
+  1. z
+  2. bb
+  3. qq
+  4. yy
+  5. yy
+  6. aac
+  7. λz.za
+-}
